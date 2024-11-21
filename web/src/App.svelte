d7896411bf97544a059f21563f85e22f0c7b9a2b
Kendree <94144671+kchenturtles@users.noreply.github.com>
2024-11-21 17:13:33 -0500

    add layout start

---
+++ web/src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import Nav from './lib/components/Nav.svelte';
+    import { currentPage } from './stores';
+    import WelcomeScreen from './pages/WelcomeScreen.svelte';
+
+    const routeHash = () => {
+        $currentPage = WelcomeScreen;
+    }
+    routeHash();
+    window.addEventListener('hashchange', routeHash);
+
+</script>
+
+{#key $currentPage}
+    <Nav />
+    <svelte:component this={$currentPage} />
+{/key}
