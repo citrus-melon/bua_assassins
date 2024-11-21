7d8cdbe9e6314ad20b843d75b99f4252a19841c1
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2024-11-21 17:26:51 -0500

    Revert "add layout start"
    This reverts commit d7896411bf97544a059f21563f85e22f0c7b9a2b.

--- web/src/stores.ts
+++
@@ -1,7 +0,0 @@
-import type { PlayerState, GameState } from '$lib/types';
-import type { Component } from 'svelte';
-import { writable } from 'svelte/store';
-
-export const currentPage = writable<Component>();
-export const playerState = writable<PlayerState>();
-export const gameState = writable<GameState>();
