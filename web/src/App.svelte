4d9d1a8766217306e29e8157121971c2467e59b4
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2024-11-21 17:20:14 -0500

    uhh I made a buncha pages

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,47 +1,60 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { quadIn } from "svelte/easing";
+  import { fly } from "svelte/transition";
+  import { currentPage } from "./stores";
+  
+    if (supabase.auth.currentSession == null) {
+      return '/welcome';
+    } else if (appStateProvider.initialized == false) {
+      return '/';
+    } else if (gameState == null || gameState == GameState.unpublished) {
+      return '/welcome';
+    } else if (playerState == PlayerState.eliminated) {
+      return '/eliminated';
+    } else if (gameState == GameState.completed) {
+      return '/game-concluded';
+    } else if (playerState == PlayerState.ineligible) {
+      return '/ineligible';
+    } else if (playerState == PlayerState.pendingRegistration) {
+      if (state.fullPath == '/registration/pre-registered' ||
+          state.fullPath == '/registration/pair-nfc') {
+        if (appStateProvider.gameState == GameState.preRegistration) {
+          return '/registration/pre-registered';
+        } else {
+          return '/registration/pair-nfc';
+        }
+      } else {
+        return '/registration';
+      }
+    } else if (gameState == GameState.paused) {
+      return '/paused';
+    } else if (gameState == GameState.registration) {
+      print(state.fullPath);
+      if (state.fullPath?.startsWith('/registered') == true) return null;
+      return '/registered';
+    } else if (gameState == GameState.active) {
+      return '/in-game';
+    } else {
+      return '/undefined-state';
+    }
 </script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+{#key $currentPage}
+  <div
+    class="wrapper"
+    out:fly={{ x: -20, duration: 200, easing: quadIn }}
+    in:fly={{ x: 32, delay: 200 }}
+  >
+    <svelte:component this={$currentPage} />
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+{/key}
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .wrapper {
+    position: absolute;
+    overflow: auto;
+    padding: 1rem;
+    width: 100%;
+    height: 100%;
   }
 </style>
