cdab27353b046932cdf5b4e2b9fc3806c9efa8be
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2024-11-21 22:53:48 -0500

    API exist

---
+++ web/src/database.types.ts
@@ -0,0 +1,275 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export type Database = {
+  public: {
+    Tables: {
+      admins: {
+        Row: {
+          created_at: string
+          game_id: number
+          user_id: string
+        }
+        Insert: {
+          created_at?: string
+          game_id: number
+          user_id: string
+        }
+        Update: {
+          created_at?: string
+          game_id?: number
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "admins_game_id_fkey"
+            columns: ["game_id"]
+            isOneToOne: false
+            referencedRelation: "games"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      games: {
+        Row: {
+          created_at: string
+          event_at: string | null
+          id: number
+          name: string
+          starts_at: string | null
+          state: Database["public"]["Enums"]["game_state"]
+        }
+        Insert: {
+          created_at?: string
+          event_at?: string | null
+          id?: number
+          name: string
+          starts_at?: string | null
+          state: Database["public"]["Enums"]["game_state"]
+        }
+        Update: {
+          created_at?: string
+          event_at?: string | null
+          id?: number
+          name?: string
+          starts_at?: string | null
+          state?: Database["public"]["Enums"]["game_state"]
+        }
+        Relationships: []
+      }
+      players: {
+        Row: {
+          created_at: string
+          died_at: string | null
+          game: number
+          id: string
+          kills: number
+          name: string | null
+          nfc_tag: string | null
+          state: Database["public"]["Enums"]["player_state"]
+          target: string | null
+        }
+        Insert: {
+          created_at?: string
+          died_at?: string | null
+          game: number
+          id: string
+          kills?: number
+          name?: string | null
+          nfc_tag?: string | null
+          state: Database["public"]["Enums"]["player_state"]
+          target?: string | null
+        }
+        Update: {
+          created_at?: string
+          died_at?: string | null
+          game?: number
+          id?: string
+          kills?: number
+          name?: string | null
+          nfc_tag?: string | null
+          state?: Database["public"]["Enums"]["player_state"]
+          target?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "players_game_fkey"
+            columns: ["game"]
+            isOneToOne: false
+            referencedRelation: "games"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "players_target_fkey"
+            columns: ["target"]
+            isOneToOne: true
+            referencedRelation: "players"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      assign_targets: {
+        Args: {
+          game_id: number
+        }
+        Returns: undefined
+      }
+      eliminate_target: {
+        Args: {
+          target_nfc_tag: string
+        }
+        Returns: {
+          created_at: string
+          died_at: string | null
+          game: number
+          id: string
+          kills: number
+          name: string | null
+          nfc_tag: string | null
+          state: Database["public"]["Enums"]["player_state"]
+          target: string | null
+        }
+      }
+      get_admin_games_for_user: {
+        Args: {
+          user_id: string
+        }
+        Returns: number[]
+      }
+      get_target_info: {
+        Args: Record<PropertyKey, never>
+        Returns: {
+          id: string
+          name: string
+          state: Database["public"]["Enums"]["player_state"]
+          game: number
+        }[]
+      }
+    }
+    Enums: {
+      game_state:
+        | "unpublished"
+        | "pre_registration"
+        | "registration"
+        | "active"
+        | "duel_phase"
+        | "completed"
+        | "paused"
+      player_state:
+        | "pending_registration"
+        | "active"
+        | "eliminated"
+        | "ineligible"
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+type PublicSchema = Database[Extract<keyof Database, "public">]
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof PublicSchema["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof PublicSchema["CompositeTypes"]
+    | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
---
+++ web/src/state.ts
@@ -0,0 +1,109 @@
+// This file manages state, API calls, data fetching, etc.
+
+import {createClient, type Session} from '@supabase/supabase-js';
+import { readable, readonly, writable, type Readable } from 'svelte/store';
+import type { Tables, Database, Enums } from './database.types';
+
+export const supabase = createClient<Database>(
+    'https://uvytjjldwcitkqattdeq.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2eXRqamxkd2NpdGtxYXR0ZGVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEyMDc0NDAsImV4cCI6MjA0Njc4MzQ0MH0.yLxvsX-brf2605syFIPR12okvRpuxjpEZM1cCDd6jMQ'
+);
+
+export const gameId = '1';
+
+export const sessionStore = readable<Session | null>(null, set => {
+    supabase.auth.getSession().then(session => {
+        set(session.data.session);
+    })
+
+    const subs = supabase.auth.onAuthStateChange((event, session) => {
+        set(session);
+    });
+
+    return () => {
+        subs.data.subscription.unsubscribe();
+    }
+});
+
+export let session: Session | null = null;
+sessionStore.subscribe(value => session = value);
+
+const internalPlayerStore = writable<Tables<'players'> | null>(null);
+const internalTargetStore = writable<{
+    id: string;
+    name: string;
+    state: Enums<'player_state'>;
+    game: number;
+} | null>(null);
+
+async function fetchTarget() {
+    const result = await supabase.rpc('get_target_info');
+    return result.data?.[0] ?? null;
+}
+
+async function fetchPlayer() {
+    const userId = session?.user.id;
+    if (!userId) return null;
+
+    const result = await supabase.from('players').select('*').eq('id', userId).single();
+    return result.data;
+}
+
+export async function refreshPlayer() {
+    internalPlayerStore.set(await fetchPlayer());
+}
+
+export async function refreshTarget() {
+    internalTargetStore.set(await fetchTarget());
+}
+
+export async function setPlayerName(name: string) {
+    const userId = session?.user.id;
+    if (!userId) return;
+
+    await supabase.from('players').update({name}).eq('id', userId);
+    refreshPlayer();
+}
+
+export async function setNfcTag(nfcTag: string) {
+    const userId = session?.user.id;
+    if (!userId) return;
+
+    await supabase.from('players').update({'nfc_tag': nfcTag}).eq('id', userId);
+    refreshPlayer();
+}
+
+export async function setPlayerState(playerState: Enums<'player_state'>) {
+    const userId = session?.user.id;
+    if (!userId) return;
+
+    await supabase.from('players').update({'state': playerState}).eq('id', userId);
+    refreshPlayer();
+}
+
+export async function eliminateTarget(nfcTag: string) {
+    const userId = session?.user.id;
+    if (!userId) return;
+
+    const result = await supabase.rpc('eliminate_target', {target_nfc_tag: nfcTag});
+    refreshPlayer();
+    refreshTarget();
+
+    return result.data;
+}
+
+export const playerStore = readonly(internalPlayerStore);
+
+const internalGameStore = writable<Tables<'games'> | null>(null);
+
+async function fetchGame() {
+    const userId = session?.user.id;
+    if (!userId) return null;
+
+    const result = await supabase.from('games').select('*').eq('id', gameId).single();
+    return result.data;
+}
+
+export async function refreshGame() {
+    internalGameStore.set(await fetchGame());
+}
