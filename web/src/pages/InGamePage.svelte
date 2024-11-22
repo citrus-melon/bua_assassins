a3d926fe4c6a4c3dff78d23ce335ebb3ee987ae5
Kendree <94144671+kchenturtles@users.noreply.github.com>
2024-11-21 22:16:54 -0500

    in game page stuff

--- web/src/pages/InGamePage.svelte
+++ web/src/pages/InGamePage.svelte
@@ -9,8 +9,9 @@
     <div class="container w-fit h-[75vh] mx-auto flex flex-col justify-between">
         <h1 class="text-center text-7xl font-bold mt-8">Kill your target!</h1>
         <Stats />
-        <div class="w-fit mx-auto">
-            <Button class="bg-[#dd0055] text-blue-950 text-2xl mb-8 p-8 rounded-full border-8 border-[#dd0055] hover:bg-blue-950 hover:text-[#dd0055]">How To Play</Button>
+        <div class="w-3/4 mx-auto grid grid-cols-1 gap-4">
+            <Button class="bg-sky-300 text-blue-950 text-2xl p-8 border-8 border-sky-300 hover:bg-blue-950 hover:text-sky-300 uppercase">Rescue</Button>
+            <Button class="bg-[#dd0055] text-blue-950 text-2xl p-8 border-8 border-[#dd0055] hover:bg-blue-950 hover:text-[#dd0055] uppercase">Kill</Button>
         </div>
     </div>
 </main>
