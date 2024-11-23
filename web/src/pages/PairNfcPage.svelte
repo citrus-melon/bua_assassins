9cfb443ba99e2c2bc11e58abae9eb77e2dd65902
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2024-11-22 20:03:31 -0500

    Strings

--- web/src/pages/PairNfcPage.svelte
+++ web/src/pages/PairNfcPage.svelte
@@ -3,13 +3,18 @@
 </script>
 
 <div class="container w-fit mx-auto">
-    <h1 class="text-center text-5xl font-bold my-8">Pair an NFC Tag!</h1>
+    <h1 class="text-center text-5xl font-bold my-8">Pair a sticker!</h1>
     <ol class="list-disc">
         <li>
-            <div class="text-2xl">Tap your phone on the tag to pair.</div>
+   
+            <div class="text-2xl">Get a sticker from Ylan or Maxwell!</div>
+
         </li>
         <li>
-            <div class="text-2xl">Place the tag on your shirt.</div>
+            <div class="text-2xl">Tap your phone on the sticker and tap on the notification to pair.</div>
+        </li>
+        <li>
+            <div class="text-2xl">Place the tag in a visible spot on your back, near your shoulders.</div>
         </li>
         <li>
             <div class="text-2xl">Do not remove the tag or cover it, or you will be disqualified.</div>
