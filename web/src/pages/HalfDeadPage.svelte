a94f3d78b9ab3844aa9822f10a183f74f20561df
Kendree <94144671+kchenturtles@users.noreply.github.com>
2024-11-22 21:32:11 -0500

    half dead

---
+++ web/src/pages/HalfDeadPage.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Button from "$lib/components/ui/button/button.svelte";
+    import Stats from "$lib/components/ui/Stats.svelte";
+    import { setPlayerState } from "../state";
+
+    const save = () => {
+        setPlayerState('active');
+    }
+    const kill = () => {
+        setPlayerState('eliminated');
+    }
+</script>
+
+<div class="container w-fit h-[75vh] mx-auto flex flex-col justify-between">
+    <div>
+        <h1 class="text-center text-5xl font-bold my-8">Was Your Death Witnessed?</h1>
+    </div>
+    <div class="grid grid-cols-2 gap-4">
+        <Button class="text-2xl rounded-full p-8" on:click={save}>Yes</Button>
+        <Button class="text-2xl rounded-full p-8" on:click={kill}>No</Button>
+    </div>
+</div>
