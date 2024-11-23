95147cdb1db7f2b579d8c910f795f753626ca97c
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2024-11-22 19:31:52 -0500

    A buton twirs jthe target

--- web/src/pages/InGamePage.svelte
+++ web/src/pages/InGamePage.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+    import { Button } from "$lib/components/ui/button";
     import Stats from "$lib/components/ui/Stats.svelte";
+    import { isTargetShown } from "../state";
 </script>
 
 <div class="container w-fit h-[75vh] mx-auto flex flex-col justify-between">
     <h1 class="text-center text-7xl font-bold mt-8">Kill your target!</h1>
     <Stats />
+    <Button onclick={() => $isTargetShown = true} class="text-2xl mb-8 rounded-full">Show Target</Button>
 </div>
