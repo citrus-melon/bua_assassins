001b828d408c2109e470cfe1459f34cff362f75c
Kendree <94144671+kchenturtles@users.noreply.github.com>
2024-11-22 21:00:28 -0500

    disable leave button

--- web/src/lib/components/ui/Nav.svelte
+++ web/src/lib/components/ui/Nav.svelte
@@ -13,7 +13,7 @@
         <img src={buaLogo} alt="BUA Logo" class="h-4"/>
         <div class="grid grid-cols-2 gap-2">
             <Button onclick={() => {refreshPlayer(); refreshGame()}} class="rounded-full">Refresh</Button>
-            <Button onclick={logout} class="rounded-full">Log Out</Button>
+            <!-- <Button onclick={logout} class="rounded-full">Log Out</Button> -->
             <!-- <Button class="bg-[#dd0055] items-center text-blue-950 rounded-full border-4 border-[#dd0055] hover:bg-blue-950 hover:text-[#dd0055]">Support</Button> -->
         </div>
     </div>
