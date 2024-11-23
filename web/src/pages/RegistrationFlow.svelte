19ec292a7441d5d9986c061b3c83ace39587a976
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2024-11-22 20:55:53 -0500

    Allow skip nfc if already exist

--- web/src/pages/RegistrationFlow.svelte
+++ web/src/pages/RegistrationFlow.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
-    import { gameStore } from "../state";
+    import { gameStore, playerStore, setPlayerState } from "../state";
 import NameInputPage from "./NameInputPage.svelte";
     import PairNfcPage from "./PairNfcPage.svelte";
     import PreregisteredPage from "./PreregisteredPage.svelte";
 
     let step = $state(0);
+
+    const thingy = async () => {
+        if ($playerStore?.nfc_tag) {
+            await setPlayerState('active');
+        }
+        step = 1;
+    }
 </script>
 
 {#if step === 0}
-    <NameInputPage nextPage={() => step = 1}/>
+    <NameInputPage nextPage={thingy}/>
 {:else}
     {#if $gameStore?.state === "pre_registration"}
         <PreregisteredPage />
