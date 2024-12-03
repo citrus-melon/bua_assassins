419a0105b026ad01f4f18a232a9c62ea2105baec
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2024-12-03 01:43:45 -0500

    Results page auto-refresh

--- results/src/routes/+layout.svelte
+++ results/src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
+    import { updated } from '$app/stores';
 	import '../app.css';
 	let { children } = $props();
+
+	$effect(() => {
+		if ($updated) {
+			location.reload();
+		}
+	})
 </script>
 
 {@render children()}
